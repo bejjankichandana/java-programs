c5baff425da5d2df46cbf09e1bc5ae6e9379eab2
Bejjanki Chandana <bejjankichandana5@gmail.com>
2026-06-21 10:10:03 +0530

    Initial commit- StudentResultManagement.java

--- StudentResultManagement.java
+++ StudentResultManagement.java
@@ -1,16 +1,12 @@
-
 import java.util.Scanner;
-
 class Student {
     String name;
     int rollNo;
     int marks1, marks2, marks3;
     double average;
     char grade;
-
     void calculateResult() {
         average = (marks1 + marks2 + marks3) / 3.0;
-
         if (average >= 90)
             grade = 'A';
         else if (average >= 75)
@@ -22,7 +18,6 @@ class Student {
         else
             grade = 'F';
     }
-
     void displayResult() {
         System.out.println("\n----- Student Result -----");
         System.out.println("Name: " + name);
@@ -32,126 +27,22 @@ class Student {
         System.out.println("Grade: " + grade);
     }
 }
-
 public class StudentResultManagement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         Student s = new Student();
-
         System.out.print("Enter Student Name: ");
         s.name = sc.nextLine();
-
         System.out.print("Enter Roll Number: ");
         s.rollNo = sc.nextInt();
-
         System.out.print("Enter Marks in Subject 1: ");
         s.marks1 = sc.nextInt();
-
         System.out.print("Enter Marks in Subject 2: ");
         s.marks2 = sc.nextInt();
-
         System.out.print("Enter Marks in Subject 3: ");
         s.marks3 = sc.nextInt();
-
         s.calculateResult();
         s.displayResult();
-
         sc.close();
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
